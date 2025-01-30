96e2ba3bc6c8c15a6a764a4a24a9304981b961c8
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2025-01-30 07:31:49 +0700

    add validation in mintDsc

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -56,6 +56,7 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__NotAllowedToken();
     error DSCEngine__TransferFailed();
     error DSCEngine__BreaksHealthFactor(uint256 healthFactor);
+    error DSCEngine__MintFailed();
 
     /**
      * State Variables
@@ -149,6 +150,10 @@ contract DSCEngine is ReentrancyGuard {
     function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
         s_DSCMinted[msg.sender] += amountDscToMint;
         _revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+        if (!minted) {
+         revert DSCEngine__MintFailed();
+        }
     }
 
     function burnDesc() external {}
