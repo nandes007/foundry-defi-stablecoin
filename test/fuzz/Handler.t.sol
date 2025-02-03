abb00cf96c78fccfe485c21229d1e13c4f6e316f
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2025-02-03 08:49:55 +0700

    add depositCollateral and redeemCollateral handler

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -6,17 +6,53 @@ pragma solidity ^0.8.18;
 import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract Handler is Test {
    DSCEngine dsce;
    DecentralizedStableCoin dsc;
 
+   ERC20Mock weth;
+   ERC20Mock wbtc;
+
+   uint256 MAX_DEPOSIT_SIZE = type(uint96).max; // the max uint96 value
+
    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
       dsce = _dscEngine;
       dsc = _dsc;
+
+      address[] memory collateralTokens = dsce.getCollateralTokens();
+      weth = ERC20Mock(collateralTokens[0]);
+      wbtc = ERC20Mock(collateralTokens[1]);
    }
 
-   function depositCollateral(address collateral, uint256 amountCollateral) public {
-      dsce.depositCollateral(collateral, amountCollateral);
+   function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+      ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+      amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+
+      vm.startPrank(msg.sender);
+      collateral.mint(msg.sender, amountCollateral);
+      collateral.approve(address(dsce), amountCollateral);
+      dsce.depositCollateral(address(collateral), amountCollateral);
+      vm.stopPrank();
+   }
+
+   function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+      ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+      uint256 maxCollateralToRedeem = dsce.getCollateralBalanceOfUser(address(collateral), msg.sender);
+      amountCollateral = bound(amountCollateral, 0, maxCollateralToRedeem);
+      if (amountCollateral == 0) {
+         return;
+      }
+      dsce.redeemCollateral(address(collateral), amountCollateral);
+   }
+
+   // Helper functions
+   function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+      if (collateralSeed % 2 == 0) {
+         return weth;
+      }
+
+      return wbtc;
    }
 }
